1fdeec04af457617d5b2f0475b1bf45ca00f561b
Siudya <32456796+Siudya@users.noreply.github.com>
2020-04-03 21:21:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -58,5 +58,5 @@ setup(
 if os.path.isdir(os.environ["PYNQ_JUPYTER_NOTEBOOKS"]+"/lstm/"):
 	shutil.rmtree(os.environ["PYNQ_JUPYTER_NOTEBOOKS"]+"/lstm/")
 shutil.copytree("notebooks/",os.environ["PYNQ_JUPYTER_NOTEBOOKS"]+"/lstm/")
-if not os.path.exists('/usr/lib/libsds_lib.so')
+if not os.path.exists('/usr/lib/libsds_lib.so'):
 	shutil.copyfile('lstm/libraries/libsds_lib.so','/usr/lib/libsds_lib.so')
